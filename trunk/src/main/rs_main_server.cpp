a53b9a9d966a3f6eeb0d8b0aab0bc90360cec2ba
hankun <hanvskun@hotmail.com>
2017-05-23 11:06:55 +0800

    refine code

--- trunk/src/main/rs_main_server.cpp
+++ trunk/src/main/rs_main_server.cpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2016 ME_Kun_Han
+Copyright (c) 2017 ME_Kun_Han
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
